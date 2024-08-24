b783f853db708ccd541de639cd55d0115474362e
Mukthabai <mukthadhinesh@gmail.com>
2024-08-24 19:26:26 +0530

    Add files via upload
    Week 2 - Day 1 Home Assignments

---
+++ day1/LaunchBrowser.java
@@ -0,0 +1,17 @@
+package week2.day1;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		
+		// To launch a browser and open facebook website
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		driver.close();
+	}
+
+}
